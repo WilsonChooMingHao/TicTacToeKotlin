6a8c7b0fcdca122f1d01722ef22d9933cf6deba1
Wilson <wilsonchoo@live.com>
2019-05-06 22:16:34 +0800

    - More Bugfix for win condition - Some UI/UX improvements for win game (e.g. color, text update)

--- app/src/main/java/com/wilsonchoominghao/tictactoekotlin/MainActivity.kt
+++ app/src/main/java/com/wilsonchoominghao/tictactoekotlin/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.wilsonchoominghao.tictactoekotlin
 
 import android.content.DialogInterface
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
@@ -11,14 +12,20 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    /*************************************************************
+     *
+     * Variables
+     *
+     *************************************************************/
+
     enum class ACTIVEPLAYER {
-        FIRST,  // CIRCLE O
-        SECOND  // CROSS X
+        FIRST,      // CIRCLE O
+        SECOND      // CROSS X
     }
 
     enum class WINNER {
-        PLAYER_ONE,
-        PLAYER_TWO,
+        PLAYER_ONE, // CIRCLE O
+        PLAYER_TWO, // CROSS X
         DRAW
     }
 
@@ -28,6 +35,13 @@ class MainActivity : AppCompatActivity() {
     var playerOneSelections: ArrayList<Int> = ArrayList()       // To track selections for winning logic
     var playerTwoSelections: ArrayList<Int> = ArrayList()       // To track selections for winning logic
     var disabledImages: ArrayList<ImageButton?> = ArrayList()   // To track all selections (for both players)
+    var allImages: ArrayList<ImageButton?> = ArrayList()        // To change color of buttons after win or draw
+
+    /*************************************************************
+     *
+     * Activity
+     *
+     *************************************************************/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,12 +49,160 @@ class MainActivity : AppCompatActivity() {
 
         // Let the CIRCLE player start
         activePlayer = ACTIVEPLAYER.FIRST
+        addAllImages()
     }
+
+    // Runs after clickImageButton, performs logic of clicking image button
+    private fun performAction(number: Int, imageButton: ImageButton) {
+        if (activePlayer == ACTIVEPLAYER.FIRST) {
+            imageButton.setImageResource(R.drawable.ic_circle_black_24dp)
+            imageButton.isEnabled = false
+            playerOneSelections.add(number)
+            disabledImages.add(imageButton)
+            activePlayer = ACTIVEPLAYER.SECOND
+            textView_CurrentPlayer.text = "CROSS (X)"
+            winnerCheck(playerOneSelections, WINNER.PLAYER_ONE)
+        } else if (activePlayer == ACTIVEPLAYER.SECOND) {
+            imageButton.setImageResource(R.drawable.ic_cross_black_24dp)
+            imageButton.isEnabled = false
+            playerTwoSelections.add(number)
+            disabledImages.add(imageButton)
+            activePlayer = ACTIVEPLAYER.FIRST
+            textView_CurrentPlayer.text = "CIRCLE (O)"
+            winnerCheck(playerTwoSelections, WINNER.PLAYER_TWO)
+        }
+    }
+
+    // Winning logic
+    private fun winnerCheck(selections: ArrayList<Int>, win: WINNER) {
+        // Checking values using hardcoded logic (top left, middle and top right)
+        // Tic Tac Toe board is using numpad / FGC style numbering e.g. hadouken is 214P or 216P
+        if (selections.contains(7)) {
+            if (selections.contains(8)) {
+                if (selections.contains(9)) {
+                    winner = win                // top row win
+                    allImages.get(7)?.setColorFilter(Color.GREEN)
+                    allImages.get(8)?.setColorFilter(Color.GREEN)
+                    allImages.get(9)?.setColorFilter(Color.GREEN)
+                }
+            }
+            if (selections.contains(4)) {
+                if (selections.contains(1)) {
+                    winner = win                // left column win
+                    allImages.get(7)?.setColorFilter(Color.GREEN)
+                    allImages.get(4)?.setColorFilter(Color.GREEN)
+                    allImages.get(1)?.setColorFilter(Color.GREEN)
+                }
+            }
+            if (selections.contains(5)) {
+                if (selections.contains(3)) {
+                    winner = win                // diagonal top left to bottom right win
+                    allImages.get(7)?.setColorFilter(Color.GREEN)
+                    allImages.get(5)?.setColorFilter(Color.GREEN)
+                    allImages.get(3)?.setColorFilter(Color.GREEN)
+                }
+            }
+        }
+        if (selections.contains(5)) {
+            if (selections.contains(4)) {
+                if (selections.contains(6)) {
+                    winner = win                // middle row win
+                    allImages.get(5)?.setColorFilter(Color.GREEN)
+                    allImages.get(4)?.setColorFilter(Color.GREEN)
+                    allImages.get(6)?.setColorFilter(Color.GREEN)
+                }
+            }
+            if (selections.contains(8)) {
+                if (selections.contains(2)) {
+                    winner = win                // middle column win
+                    allImages.get(5)?.setColorFilter(Color.GREEN)
+                    allImages.get(8)?.setColorFilter(Color.GREEN)
+                    allImages.get(2)?.setColorFilter(Color.GREEN)
+                }
+            }
+            if (selections.contains(9)) {
+                if (selections.contains(1)) {
+                    winner = win                // diagonal top right to bottom left win
+                    allImages.get(5)?.setColorFilter(Color.GREEN)
+                    allImages.get(9)?.setColorFilter(Color.GREEN)
+                    allImages.get(1)?.setColorFilter(Color.GREEN)
+                }
+            }
+        }
+        if (selections.contains(3)) {
+            if (selections.contains(2)) {
+                if (selections.contains(1)) {
+                    winner = win                // bottom row win
+                    allImages.get(3)?.setColorFilter(Color.GREEN)
+                    allImages.get(2)?.setColorFilter(Color.GREEN)
+                    allImages.get(1)?.setColorFilter(Color.GREEN)
+                }
+            }
+            if (selections.contains(6)) {
+                if (selections.contains(9)) {
+                    winner = win                // right column win
+                    allImages.get(3)?.setColorFilter(Color.GREEN)
+                    allImages.get(6)?.setColorFilter(Color.GREEN)
+                    allImages.get(9)?.setColorFilter(Color.GREEN)
+                }
+            }
+        }
+
+        // Check if anyone won or draw game
+        if (winner == WINNER.PLAYER_ONE) {
+            createWinnerAlert("Player One Wins!", "Congrats Circle O!", AlertDialog.BUTTON_POSITIVE, "OK", false)
+        } else if (winner == WINNER.PLAYER_TWO) {
+            createWinnerAlert("Player Two Wins!", "Congrats Cross X!", AlertDialog.BUTTON_POSITIVE, "OK", false)
+        } else if (disabledImages.size == 9) {
+            createWinnerAlert("Draw Game!", "Nobody wins but nobody loses!", AlertDialog.BUTTON_POSITIVE, "OK", false)
+        }
+    }
+
+    // Display winner
+    private fun createWinnerAlert(title: String, message: String, buttonType: Int, buttonText: String, cancelable: Boolean) {
+        val alertDialog: AlertDialog = AlertDialog.Builder(this).create()
+        alertDialog.setTitle(title)
+        alertDialog.setMessage(message)
+        alertDialog.setCancelable(cancelable)
+
+        alertDialog.setButton(buttonType, buttonText, { dialog: DialogInterface?, which: Int ->
+            null
+        })
+        textView_CurrentPlayerTitle.text = "GAME OVER: "
+        textView_CurrentPlayer.text = title
+        alertDialog.show()
+    }
+
+    /*************************************************************
+     *
+     * Helper Functions
+     *
+     *************************************************************/
+
+    private fun addAllImages() {
+        // Add null to 0 for easy processing
+        allImages.add(null)
+        allImages.add(imageButton_1)
+        allImages.add(imageButton_2)
+        allImages.add(imageButton_3)
+        allImages.add(imageButton_4)
+        allImages.add(imageButton_5)
+        allImages.add(imageButton_6)
+        allImages.add(imageButton_7)
+        allImages.add(imageButton_8)
+        allImages.add(imageButton_9)
+    }
+
+    /*************************************************************
+     *
+     * OnClick Functions
+     *
+     *************************************************************/
 
     // Called when clicking image button (i.e the tic tac toe spaces)
     fun clickImageButton(view: View) {
         // Only allow logic processing if nobody has won and not draw
-        if(winner == null) {
+        if (winner == null) {
             val selectedImageButton: ImageButton = view as ImageButton
 
             var number: Int = 0
@@ -61,100 +223,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // Login of clicking image button
-    private fun performAction(number: Int, imageButton: ImageButton) {
-        if (activePlayer == ACTIVEPLAYER.FIRST) {
-            imageButton.setImageResource(R.drawable.ic_circle_black_24dp)
-            imageButton.isEnabled = false
-            playerOneSelections.add(number)
-            disabledImages.add(imageButton)
-            activePlayer = ACTIVEPLAYER.SECOND
-            textView_CurrentPlayer.text = "CROSS (X)"
-            winnerCheck(playerOneSelections, WINNER.PLAYER_ONE)
-        } else if (activePlayer == ACTIVEPLAYER.SECOND) {
-            imageButton.setImageResource(R.drawable.ic_cross_black_24dp)
-            imageButton.isEnabled = false
-            playerTwoSelections.add(number)
-            disabledImages.add(imageButton)
-            activePlayer = ACTIVEPLAYER.FIRST
-            textView_CurrentPlayer.text = "CIRCLE (O)"
-            winnerCheck(playerTwoSelections, WINNER.PLAYER_TWO)
-        }
-
-    }
-
-    private fun winnerCheck(selections: ArrayList<Int>, win: WINNER) {
-        // Checking values using hardcoded logic (top left, middle and top right)
-        // Tic Tac Toe board is using numpad / FGC style numbering e.g. hadouken is 214P or 216P
-        if (selections.contains(7)) {
-            if (selections.contains(8)) {
-                if (selections.contains(9)) {
-                    winner = win                // top row win
-                }
-            } else if (selections.contains(4)) {
-                if (selections.contains(1)) {
-                    winner = win                // left column win
-                }
-            } else if (selections.contains(5)) {
-                if (selections.contains(3)) {
-                    winner = win                // diagonal top left to bottom right win
-                }
-            }
-        }
-        if (selections.contains(5)) {
-            if (selections.contains(4)) {
-                if (selections.contains(6)) {
-                    winner = win                // middle row win
-                }
-            } else if (selections.contains(8)) {
-                if (selections.contains(2)) {
-                    winner = win                // middle column win
-                }
-            } else if (selections.contains(9)) {
-                if (selections.contains(1)) {
-                    winner = win                // diagonal top right to bottom left win
-                }
-            }
-        }
-        if (selections.contains(3)) {
-            if (selections.contains(2)) {
-                if (selections.contains(1)) {
-                    winner = win                // bottom row win
-                }
-            } else if (selections.contains(6)) {
-                if (selections.contains(9)) {
-                    winner = win                // right column win
-                }
-            }
-        }
-
-        if (winner == WINNER.PLAYER_ONE) {
-            createWinnerAlert("Player One Wins!", "Congrats Circle O!", AlertDialog.BUTTON_POSITIVE, "OK", false)
-        } else if (winner == WINNER.PLAYER_TWO) {
-            createWinnerAlert("Player Two Wins!", "Congrats Cross X!", AlertDialog.BUTTON_POSITIVE, "OK", false)
-        } else if (disabledImages.size == 9) {
-            createWinnerAlert("Draw Game!", "Nobody wins but nobody loses!", AlertDialog.BUTTON_POSITIVE, "OK", false)
-        }
-    }
-
-    // Display winner
-    private fun createWinnerAlert(title: String, message: String, buttonType: Int, buttonText: String, cancelable: Boolean) {
-        val alertDialog: AlertDialog = AlertDialog.Builder(this).create()
-        alertDialog.setTitle(title)
-        alertDialog.setMessage(message)
-        alertDialog.setCancelable(cancelable)
-
-        alertDialog.setButton(buttonType, buttonText, { dialog: DialogInterface?, which: Int ->
-            null
-        })
-
-        alertDialog.show()
-    }
-
     // Reset game state to start
-    fun resetGame(view: View) {
+    fun clickResetGame(view: View) {
         winner = null
         activePlayer = ACTIVEPLAYER.FIRST
+        textView_CurrentPlayerTitle.text = "Current Player: "
         textView_CurrentPlayer.text = "CIRCLE (O)"
 
         playerOneSelections.clear()
@@ -162,6 +235,7 @@ class MainActivity : AppCompatActivity() {
         for (i in disabledImages.indices) {
             disabledImages[i]?.setImageResource(0)
             disabledImages[i]?.isEnabled = true
+            disabledImages[i]?.setColorFilter(0)
         }
         disabledImages.clear()
 
