be087eda1284de1b3f350738802ddcc544fc60f0
Wilson <wilsonchoo@live.com>
2019-05-06 22:27:38 +0800

    - Bugfix: Don't need ArrayList for all imagebuttons since we can just use the ids - Changed naming scene of enums to make better sense

--- app/src/main/java/com/wilsonchoominghao/tictactoekotlin/MainActivity.kt
+++ app/src/main/java/com/wilsonchoominghao/tictactoekotlin/MainActivity.kt
@@ -19,13 +19,13 @@ class MainActivity : AppCompatActivity() {
      *************************************************************/
 
     enum class ACTIVEPLAYER {
-        FIRST,      // CIRCLE O
-        SECOND      // CROSS X
+        CIRCLE,      // CIRCLE O
+        CROSS      // CROSS X
     }
 
     enum class WINNER {
-        PLAYER_ONE, // CIRCLE O
-        PLAYER_TWO, // CROSS X
+        CIRCLE, // CIRCLE O
+        CROSS, // CROSS X
         DRAW
     }
 
@@ -35,7 +35,6 @@ class MainActivity : AppCompatActivity() {
     var playerOneSelections: ArrayList<Int> = ArrayList()       // To track selections for winning logic
     var playerTwoSelections: ArrayList<Int> = ArrayList()       // To track selections for winning logic
     var disabledImages: ArrayList<ImageButton?> = ArrayList()   // To track all selections (for both players)
-    var allImages: ArrayList<ImageButton?> = ArrayList()        // To change color of buttons after win or draw
 
     /*************************************************************
      *
@@ -48,28 +47,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Let the CIRCLE player start
-        activePlayer = ACTIVEPLAYER.FIRST
-        addAllImages()
+        activePlayer = ACTIVEPLAYER.CIRCLE
     }
 
     // Runs after clickImageButton, performs logic of clicking image button
     private fun performAction(number: Int, imageButton: ImageButton) {
-        if (activePlayer == ACTIVEPLAYER.FIRST) {
+        if (activePlayer == ACTIVEPLAYER.CIRCLE) {
             imageButton.setImageResource(R.drawable.ic_circle_black_24dp)
             imageButton.isEnabled = false
             playerOneSelections.add(number)
             disabledImages.add(imageButton)
-            activePlayer = ACTIVEPLAYER.SECOND
+            activePlayer = ACTIVEPLAYER.CROSS
             textView_CurrentPlayer.text = "CROSS (X)"
-            winnerCheck(playerOneSelections, WINNER.PLAYER_ONE)
-        } else if (activePlayer == ACTIVEPLAYER.SECOND) {
+            winnerCheck(playerOneSelections, WINNER.CIRCLE)
+        } else if (activePlayer == ACTIVEPLAYER.CROSS) {
             imageButton.setImageResource(R.drawable.ic_cross_black_24dp)
             imageButton.isEnabled = false
             playerTwoSelections.add(number)
             disabledImages.add(imageButton)
-            activePlayer = ACTIVEPLAYER.FIRST
+            activePlayer = ACTIVEPLAYER.CIRCLE
             textView_CurrentPlayer.text = "CIRCLE (O)"
-            winnerCheck(playerTwoSelections, WINNER.PLAYER_TWO)
+            winnerCheck(playerTwoSelections, WINNER.CROSS)
         }
     }
 
@@ -81,25 +79,25 @@ class MainActivity : AppCompatActivity() {
             if (selections.contains(8)) {
                 if (selections.contains(9)) {
                     winner = win                // top row win
-                    allImages.get(7)?.setColorFilter(Color.GREEN)
-                    allImages.get(8)?.setColorFilter(Color.GREEN)
-                    allImages.get(9)?.setColorFilter(Color.GREEN)
+                    imageButton_7.setColorFilter(Color.GREEN)
+                    imageButton_8.setColorFilter(Color.GREEN)
+                    imageButton_9.setColorFilter(Color.GREEN)
                 }
             }
             if (selections.contains(4)) {
                 if (selections.contains(1)) {
                     winner = win                // left column win
-                    allImages.get(7)?.setColorFilter(Color.GREEN)
-                    allImages.get(4)?.setColorFilter(Color.GREEN)
-                    allImages.get(1)?.setColorFilter(Color.GREEN)
+                    imageButton_7.setColorFilter(Color.GREEN)
+                    imageButton_4.setColorFilter(Color.GREEN)
+                    imageButton_1.setColorFilter(Color.GREEN)
                 }
             }
             if (selections.contains(5)) {
                 if (selections.contains(3)) {
                     winner = win                // diagonal top left to bottom right win
-                    allImages.get(7)?.setColorFilter(Color.GREEN)
-                    allImages.get(5)?.setColorFilter(Color.GREEN)
-                    allImages.get(3)?.setColorFilter(Color.GREEN)
+                    imageButton_7.setColorFilter(Color.GREEN)
+                    imageButton_5.setColorFilter(Color.GREEN)
+                    imageButton_3.setColorFilter(Color.GREEN)
                 }
             }
         }
@@ -107,25 +105,25 @@ class MainActivity : AppCompatActivity() {
             if (selections.contains(4)) {
                 if (selections.contains(6)) {
                     winner = win                // middle row win
-                    allImages.get(5)?.setColorFilter(Color.GREEN)
-                    allImages.get(4)?.setColorFilter(Color.GREEN)
-                    allImages.get(6)?.setColorFilter(Color.GREEN)
+                    imageButton_5.setColorFilter(Color.GREEN)
+                    imageButton_4.setColorFilter(Color.GREEN)
+                    imageButton_6.setColorFilter(Color.GREEN)
                 }
             }
             if (selections.contains(8)) {
                 if (selections.contains(2)) {
                     winner = win                // middle column win
-                    allImages.get(5)?.setColorFilter(Color.GREEN)
-                    allImages.get(8)?.setColorFilter(Color.GREEN)
-                    allImages.get(2)?.setColorFilter(Color.GREEN)
+                    imageButton_5.setColorFilter(Color.GREEN)
+                    imageButton_8.setColorFilter(Color.GREEN)
+                    imageButton_2.setColorFilter(Color.GREEN)
                 }
             }
             if (selections.contains(9)) {
                 if (selections.contains(1)) {
                     winner = win                // diagonal top right to bottom left win
-                    allImages.get(5)?.setColorFilter(Color.GREEN)
-                    allImages.get(9)?.setColorFilter(Color.GREEN)
-                    allImages.get(1)?.setColorFilter(Color.GREEN)
+                    imageButton_5.setColorFilter(Color.GREEN)
+                    imageButton_9.setColorFilter(Color.GREEN)
+                    imageButton_1.setColorFilter(Color.GREEN)
                 }
             }
         }
@@ -133,26 +131,26 @@ class MainActivity : AppCompatActivity() {
             if (selections.contains(2)) {
                 if (selections.contains(1)) {
                     winner = win                // bottom row win
-                    allImages.get(3)?.setColorFilter(Color.GREEN)
-                    allImages.get(2)?.setColorFilter(Color.GREEN)
-                    allImages.get(1)?.setColorFilter(Color.GREEN)
+                    imageButton_3.setColorFilter(Color.GREEN)
+                    imageButton_2.setColorFilter(Color.GREEN)
+                    imageButton_1.setColorFilter(Color.GREEN)
                 }
             }
             if (selections.contains(6)) {
                 if (selections.contains(9)) {
                     winner = win                // right column win
-                    allImages.get(3)?.setColorFilter(Color.GREEN)
-                    allImages.get(6)?.setColorFilter(Color.GREEN)
-                    allImages.get(9)?.setColorFilter(Color.GREEN)
+                    imageButton_3.setColorFilter(Color.GREEN)
+                    imageButton_6.setColorFilter(Color.GREEN)
+                    imageButton_9.setColorFilter(Color.GREEN)
                 }
             }
         }
 
         // Check if anyone won or draw game
-        if (winner == WINNER.PLAYER_ONE) {
-            createWinnerAlert("Player One Wins!", "Congrats Circle O!", AlertDialog.BUTTON_POSITIVE, "OK", false)
-        } else if (winner == WINNER.PLAYER_TWO) {
-            createWinnerAlert("Player Two Wins!", "Congrats Cross X!", AlertDialog.BUTTON_POSITIVE, "OK", false)
+        if (winner == WINNER.CIRCLE) {
+            createWinnerAlert("Circle (O) Wins!", "Congrats!", AlertDialog.BUTTON_POSITIVE, "OK", false)
+        } else if (winner == WINNER.CROSS) {
+            createWinnerAlert("Cross (X) Wins!", "Congrats!", AlertDialog.BUTTON_POSITIVE, "OK", false)
         } else if (disabledImages.size == 9) {
             createWinnerAlert("Draw Game!", "Nobody wins but nobody loses!", AlertDialog.BUTTON_POSITIVE, "OK", false)
         }
@@ -171,26 +169,6 @@ class MainActivity : AppCompatActivity() {
         textView_CurrentPlayerTitle.text = "GAME OVER: "
         textView_CurrentPlayer.text = title
         alertDialog.show()
-    }
-
-    /*************************************************************
-     *
-     * Helper Functions
-     *
-     *************************************************************/
-
-    private fun addAllImages() {
-        // Add null to 0 for easy processing
-        allImages.add(null)
-        allImages.add(imageButton_1)
-        allImages.add(imageButton_2)
-        allImages.add(imageButton_3)
-        allImages.add(imageButton_4)
-        allImages.add(imageButton_5)
-        allImages.add(imageButton_6)
-        allImages.add(imageButton_7)
-        allImages.add(imageButton_8)
-        allImages.add(imageButton_9)
     }
 
     /*************************************************************
@@ -226,7 +204,7 @@ class MainActivity : AppCompatActivity() {
     // Reset game state to start
     fun clickResetGame(view: View) {
         winner = null
-        activePlayer = ACTIVEPLAYER.FIRST
+        activePlayer = ACTIVEPLAYER.CIRCLE
         textView_CurrentPlayerTitle.text = "Current Player: "
         textView_CurrentPlayer.text = "CIRCLE (O)"
 
